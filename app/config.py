919933809e4107e72ee3790dfdabb1985c932282
Haris Prasetya D <haris.situmeang.1412@gmail.com>
2024-02-13 23:15:45 +0700

    feat(auth): add simple auth feature to app

--- app/config.py
+++ app/config.py
@@ -1,7 +1,29 @@
+import secrets
 from os import getenv
 
+ALGORITHM = "HS256"
+SECRET_KEY = secrets.token_urlsafe(32)
+
 PARK_CAPACITY=int(getenv('PARK_CAPACITY', 'default'))
+ACCESS_TOKEN_EXPIRE_MINUTES=int(getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 'default'))
 
 if not PARK_CAPACITY:
-    print("Environment variable not set or empty. Using default value.")
+    print("PARK_CAPACITY not set or empty. Using default value.")
     PARK_CAPACITY = 3
+
+if not ACCESS_TOKEN_EXPIRE_MINUTES:
+    print("ACCESS_TOKEN_EXPIRE_MINUTES not set or empty. Using default value.")
+    ACCESS_TOKEN_EXPIRE_MINUTES = 5
+
+USERS_DB = {
+    "user": {
+        "username": "user",
+        "password": "passwd",
+        "role": "user"
+    },
+    "admin": {
+        "username": "admin",
+        "password": "root",
+        "role": "admin"
+    }
+}
--- app/main.py
+++ app/main.py
@@ -1,27 +1,83 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
 from typing import Dict
 
 from domain.car import Car, CarPark
-from config import PARK_CAPACITY
+from config import ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, \
+SECRET_KEY, PARK_CAPACITY, USERS_DB
+
 
 app = FastAPI()
-
-
 car_park = CarPark(capacity=PARK_CAPACITY)
+users_db = USERS_DB
+
+def authenticate_user(username: str, password: str):
+    user = users_db.get(username)
+    if not user:
+        return False
+    if not password == user["password"]:
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Invalid credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    return username
+
+@app.post("/token")
+async def login(form_data: dict):
+    user = authenticate_user(form_data["username"], form_data["password"])
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": form_data["username"]}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @app.post("/car/enter")
-def enter_car(car_id: str) -> str:
+def enter_car(car_id: str, current_user: str = Depends(get_current_user)) -> str:
+    user_role = current_user.get("role")
+    if user_role != "admin":
+        raise HTTPException(status_code=403, detail="Permission denied")
     car = Car(car_id)
     return car_park.car_arrived(car.car_id)
 
 @app.post("/car/exit")
-def exit_car() -> str:
+def exit_car(current_user: str = Depends(get_current_user)) -> str:
+    user_role = current_user.get("role")
+    if user_role != "admin":
+        raise HTTPException(status_code=403, detail="Permission denied")
     car_id, message = car_park.car_departed()
     if car_id:
         return f"Car {car_id} exited the car park. " + message
     else:
         return message
+
 @app.get("/carpark/status")
 def carpark_status() -> Dict:
     return {
