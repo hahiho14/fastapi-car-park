f961766bd250cd2a07cbf173a781326cf0f5148e
Haris Prasetya D <haris.situmeang.1412@gmail.com>
2024-02-13 23:36:32 +0700

    docs(app): add docstring in code

--- app/main.py
+++ app/main.py
@@ -14,7 +14,15 @@ app = FastAPI()
 car_park = CarPark(capacity=PARK_CAPACITY)
 users_db = USERS_DB
 
-def authenticate_user(username: str, password: str):
+def authenticate_user(username: str, password: str) -> Dict:
+    """
+    Authenticate user from user database.
+    Args:
+        username(str): username user
+        password(str): password user
+    Returns:
+        Dict: Return Dict of user
+    """
     user = users_db.get(username)
     if not user:
         return False
@@ -22,7 +30,15 @@ def authenticate_user(username: str, password: str):
         return False
     return user
 
-def create_access_token(data: dict, expires_delta: timedelta):
+def create_access_token(data: dict, expires_delta: timedelta) -> str:
+    """
+    Create access token with expiration date.
+    Args:
+        data(dict): data to encode with jwt
+        expires_delta(timedelta): expiration delta in minutes
+    Returns:
+        str: Return jwt token
+    """
     to_encode = data.copy()
     expire = datetime.utcnow() + expires_delta
     to_encode.update({"exp": expire})
@@ -31,7 +47,14 @@ def create_access_token(data: dict, expires_delta: timedelta):
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme)) -> str:
+    """
+    Get current user that have been logged in.
+    Args:
+        token(str): jwt token that want to decode
+    Returns:
+        str: Return username that have been logged in
+    """
     credentials_exception = HTTPException(
         status_code=401,
         detail="Invalid credentials",
@@ -47,7 +70,14 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     return username
 
 @app.post("/token")
-async def login(form_data: dict):
+async def login(form_data: dict) -> Dict:
+    """
+    Login user and create access token.
+    Args:
+        form_data(dict): username and password dict
+    Returns:
+        Dict: Return access token and token type
+    """
     user = authenticate_user(form_data["username"], form_data["password"])
     if not user:
         raise HTTPException(
@@ -61,6 +91,14 @@ async def login(form_data: dict):
 
 @app.post("/car/enter")
 def enter_car(car_id: str, current_user: str = Depends(get_current_user)) -> str:
+    """
+    Enter new car to the Car Park.
+    Args:
+        car_id(str): id of a car
+        current_user(str): current user logged in
+    Returns:
+        str: Return messages that car enter car park or put in queue
+    """
     user_role = current_user.get("role")
     if user_role != "admin":
         raise HTTPException(status_code=403, detail="Permission denied")
@@ -69,6 +107,13 @@ def enter_car(car_id: str, current_user: str = Depends(get_current_user)) -> str
 
 @app.post("/car/exit")
 def exit_car(current_user: str = Depends(get_current_user)) -> str:
+    """
+    A car exit the Car Park.
+    Args:
+        current_user(str): current user logged in
+    Returns:
+        str: Return messages that car exit and new car remove from queue
+    """
     user_role = current_user.get("role")
     if user_role != "admin":
         raise HTTPException(status_code=403, detail="Permission denied")
@@ -80,6 +125,13 @@ def exit_car(current_user: str = Depends(get_current_user)) -> str:
 
 @app.get("/carpark/status")
 def carpark_status() -> Dict:
+    """
+    Check Car Park status.
+    Args:
+        -
+    Returns:
+        Dict: Return parking spaces and queue size
+    """
     return {
         "parking_spaces": car_park.parking_spaces,
         "queue_size": len(car_park.queue)
